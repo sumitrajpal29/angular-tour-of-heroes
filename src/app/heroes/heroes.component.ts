dfe878dfdf3d3170c83afed916504f9da3b55954
sumitrajpal29 <sumitrajpal29@users.noreply.github.com>
2023-03-04 02:48:18 +0530

    day four

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,14 +12,23 @@ export class HeroesComponent implements OnInit {
 
   @Output() selectedHero?: Hero;
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
+  constructor(private heroService: HeroService, private messageService:MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+
+    //This is for getting heroes data from heroService asynchronously
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
   }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
 }
